6f9ee052b83e9ca64e0af80cccf8512e1c8f7cf3
Scott Susanto <scottsusanto@yahoo.com>
2022-11-11 20:15:24 -0800

    add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/test.tsx',
+    devtool: 'cheap-module-source-map',
+    entry: {
+        popup: path.resolve('src/popup/popup.tsx'),
+    },
     module: {
         rules: [
             {
@@ -12,11 +17,28 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve('src/manifest.json'),
+                    to: path.resolve('dist'),
+                }
+            ]
+        }),
+        new HtmlPlugin({
+            title: 'Hello Spotlight Popup',
+            filename: 'popup.html',
+            chunks: [
+                'popup', // a JS file turned into a chunk
+            ]
+        })
+    ],
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        filename: 'index.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     }
 }
