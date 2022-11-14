7c52f32d21375bc89118d39fc932579733d2e07b
Scott Susanto <scottsus@usc.edu>
2022-11-14 00:56:27 -0800

    stubhub takes into account section, row, price

--- server/stubhub.py
+++ server/stubhub.py
@@ -9,11 +9,12 @@ from driver_options import chrome_options
 from price_parser import Price
 
 # Stubhub specific webscraping
-def scrape(team1, team2):
+def scrape(team1, team2, section, row, price):
     url = 'https://stubhub.com'
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.get(url)
-    print("Finding cheaper tickets for " + team1 + " vs " + team2 + " from " + url)
+    print("Finding cheaper tickets for " + team1 + " vs " + team2 + " at " + "Section: " 
+        + str(section) + ", Row: " + str(row) + " from " + url)
 
     search_bar = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located(
@@ -40,27 +41,39 @@ def scrape(team1, team2):
     # print("Chose 1 ticket", driver.current_url)
 
     WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable(
+        EC.presence_of_element_located(
             (By.XPATH, "//div[div[contains(text(), 'Section') and span[contains(text(), 'Row')]]]")
         )
-    ).click()
-    # print("Section: X", driver.current_url)
+    )
 
-    you_pay_div = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//div[span[contains(text(), 'each')]]")
-        )
-    ) # because there are multiple $ in the page but only 1 'each'
-    price_span = you_pay_div.find_element(By.XPATH, ".//span[contains(text(), '$')]")
-    price = Price.fromstring(price_span.text).amount_text
-    
-    row_div = driver.find_element(By.XPATH, "//div/div[contains(text(), 'Row')]")
-    row = row_div.text
+    choices = driver.find_elements(By.XPATH, 
+        "//div[div/div[contains(text(), 'Section') and span[contains(text(), 'Row')]]]"
+    )
+
+    for choice in choices:
+        (ok, this_row, this_price) = assess(choice, section, row, price)
+        if (ok):
+            choice.click()
+            url = driver.current_url
+            driver.close()
+            return ('stubhub', this_row, this_price, url)        
+
+    (_, this_row, this_price) = assess(choices[0], section, row, price)
+    choices[0].click()
     url = driver.current_url
-    print(url)
-
-    print(row)
-    print(price)
     driver.close()
-    return ('stubhub', row, price, url)
+    return ('stubhub', this_row, this_price, url)
+
     
+def assess(choice, section, row, price):
+    (this_section_num, this_row_num, this_price) = parse_seats(choice.text)
+    if this_section_num != int(section) or this_row_num >= int(row) or this_price >= float(price):
+        return (False, 0, 0)
+    return (True, this_row_num, this_price)
+
+def parse_seats(text):
+    digits = [int(s) for s in text.split() if s.isdigit()]
+    this_price = Price.fromstring(text.split('$')[1]).amount_float
+    return (digits[0], digits[1], this_price)
+
+scrape('Los Angeles Clippers', 'Minnesota Timberwolves', 309, 7, 40)
