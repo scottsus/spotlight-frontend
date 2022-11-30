ffea41ecf2b13aa7e1af1eee57edbac2fc233bb9
dennis <dwang984@usc.edu>
2022-11-29 18:58:28 -0800

    vividseats

--- server/test_server.py
+++ server/test_server.py
@@ -36,7 +36,7 @@ def scrape_seatgeek():
             "row": dest_row2,
             "totalPrice": dest_price2,
             "quantity": quantity,
-            "url": "https://seatgeek.com/2"
+            "url": "https://seatgeek.com"
         }
     ])
 
@@ -132,6 +132,40 @@ def scrape_ticketmaster():
         },
         {
             "name": "ticketmaster",
+            "section": src_section,
+            "row": dest_row2,
+            "totalPrice": dest_price2,
+            "quantity": quantity,
+            "url": "https://ticketmaster.com/2"
+        }
+    ])
+
+@app.route("/scrape/vividseats", methods=["GET"])
+def scrape_vividseats():
+    timeWait = random.randint(3, 10)
+    time.sleep(timeWait)
+    (team1, team2, src_section, src_row, src_price, quantity) = get_data_from_req(request)
+    dest_price_low = 0 if int(float(src_price)) - 300 < 0 else 300
+    dest_price1 = random.randint(dest_price_low, int(float(src_price)))
+    dest_price2 = random.randint(dest_price_low, int(float(src_price)))
+    if dest_price1 % 2 == 1:
+        dest_price1 += 1
+    if dest_price2 % 2 == 1:
+        dest_price2 += 1
+    dest_row1 = random.randint(1, int(src_row))
+    dest_row2 = random.randint(1, int(src_row))
+    
+    return json.dumps([
+        {
+            "name": "vividseats",
+            "section": src_section,
+            "row": dest_row1,
+            "totalPrice": dest_price1,
+            "quantity": quantity,
+            "url": "https://ticketmaster.com"
+        },
+        {
+            "name": "vividseats",
             "section": src_section,
             "row": dest_row2,
             "totalPrice": dest_price2,
